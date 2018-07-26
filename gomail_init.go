37b41501b607fe2694b1a3d5e92b40e3860b3465
Jenazads <felipe.moreno.vera@gmail.com>
2018-07-26 04:59:21 -0500

    init mail

--- gomail_init.go
+++ gomail_init.go
@@ -3,7 +3,6 @@ package gomail
 import "fmt"
 
 func init(){
-  fmt.Println("\n\n====> GoMail version 1.1.3 last update 09/04/2018 \n")
-  fmt.Println("====> Init the GoMail SMTP Library in Golang :D\n")
-  fmt.Println("====> by Jenazads.\n\n")
+  fmt.Println("====> Init GoMail, a SMTP Library :D")
+  fmt.Println("====> by Jenazads.\n")
 }
